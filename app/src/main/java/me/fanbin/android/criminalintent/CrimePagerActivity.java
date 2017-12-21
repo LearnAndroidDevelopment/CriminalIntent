9a579fb2c38e4da3bc1289fb0b010bfafaa64498
Fan Bin <fanbin0602@163.com>
2017-12-21 14:19:12 +0800

    practice-11

--- app/src/main/java/me/fanbin/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/me/fanbin/android/criminalintent/CrimePagerActivity.java
@@ -8,6 +8,9 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +20,9 @@ public class CrimePagerActivity extends AppCompatActivity {
     private static final String EXTRA_CRIME_ID = "me.fanbin.android.criminalintent.crime_id";
 
     private ViewPager mViewPager;
+    private Button mPrevButton;
+    private Button mNextButton;
+
     private List<Crime> mCrimes;
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
@@ -47,6 +53,33 @@ public class CrimePagerActivity extends AppCompatActivity {
             public int getCount() {
                 return mCrimes.size();
             }
+
+            @Override
+            public void finishUpdate(ViewGroup container) {
+                super.finishUpdate(container);
+                updateButtonEnable();
+            }
+        });
+
+        mPrevButton = findViewById(R.id.prev_button);
+        mNextButton = findViewById(R.id.next_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mViewPager.getCurrentItem() != 0) {
+                    mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
+                }
+                updateButtonEnable();
+            }
+        });
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mViewPager.getCurrentItem() != mCrimes.size() - 1) {
+                    mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1);
+                }
+                updateButtonEnable();
+            }
         });
 
         for (int i = 0; i < mCrimes.size(); i++) {
@@ -55,5 +88,11 @@ public class CrimePagerActivity extends AppCompatActivity {
                 break;
             }
         }
+        updateButtonEnable();
+    }
+
+    private void updateButtonEnable() {
+        mPrevButton.setEnabled(mViewPager.getCurrentItem() != 0);
+        mNextButton.setEnabled(mViewPager.getCurrentItem() != mCrimes.size() - 1);
     }
 }
