451029971a2c6d3772491c964afac53737f76e17
Fan Bin <fanbin0602@163.com>
2017-12-26 15:35:32 +0800

    SQLite

--- app/src/main/java/me/fanbin/android/criminalintent/CrimeLab.java
+++ app/src/main/java/me/fanbin/android/criminalintent/CrimeLab.java
@@ -1,10 +1,19 @@
 package me.fanbin.android.criminalintent;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import me.fanbin.android.criminalintent.database.CrimeBaseHelper;
+import me.fanbin.android.criminalintent.database.CrimeCursorWrapper;
+import me.fanbin.android.criminalintent.database.CrimeDbSchema;
+import me.fanbin.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 /**
  * Created by fanbin on 2017/12/18.
@@ -15,6 +24,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -24,24 +35,72 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+        mDatabase.update(CrimeTable.NAME, values, CrimeTable.Cols.UUID + " = ?", new String[]{uuidString});
+    }
+
+    private CrimeCursorWrapper queryCrimes(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null, // Column - null selects all columns.
+                whereClause,
+                whereArgs,
+                null, // group by
+                null, // having
+                null // order by
+        );
+        return new CrimeCursorWrapper(cursor);
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        List<Crime> crimes = new ArrayList<>();
+        CrimeCursorWrapper cursor = queryCrimes(null, null);
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+        return crimes;
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
+        CrimeCursorWrapper cursor = queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { id.toString() }
+        );
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
             }
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
         }
-        return null;
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        return values;
     }
 
 }
---
+++ app/src/main/java/me/fanbin/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,19 @@
+package me.fanbin.android.criminalintent.database;
+
+/**
+ * Created by fanbin on 2017/12/26.
+ */
+
+public class CrimeDbSchema {
+
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+
+}
