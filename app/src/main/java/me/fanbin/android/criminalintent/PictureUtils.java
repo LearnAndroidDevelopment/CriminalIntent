1e79bc49fc6476000712e5d9c894face61670878
Fan Bin <fanbin0602@163.com>
2017-12-27 15:32:20 +0800

    camera

---
+++ app/src/main/java/me/fanbin/android/criminalintent/PictureUtils.java
@@ -0,0 +1,44 @@
+package me.fanbin.android.criminalintent;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+
+/**
+ * Created by Administrator on 2017/12/27.
+ */
+
+public class PictureUtils {
+
+    public static Bitmap getScaledBitmap(String path, int destWith, int destHeight) {
+        // Read in the dimensions of the image on disk.
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        // Figure out how much to scale down by
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWith) {
+            float heightScale = srcHeight / destHeight;
+            float widthScale = srcWidth / destWith;
+            inSampleSize = Math.round(heightScale > widthScale ? heightScale : widthScale);
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        // Read in and create final bitmap
+        return BitmapFactory.decodeFile(path, options);
+    }
+
+    public static Bitmap getScaleBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay().getSize(size);
+        return getScaledBitmap(path, size.x, size.y);
+    }
+
+}
